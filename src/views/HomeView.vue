8ddef51b150784a6490d784d60d82f26a6764188
GopiAjt <gopiajt23@gmail.com>
2024-09-05 07:46:04 +0530

    add: logic to fetch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,47 @@
 <template>
   <label for="link">input url</label>
-    <input type="text">
+  <input v-model="link" type="text">
+  <button @click="download">submit</button>
 </template>
-<script>
 
+<script>
+export default {
+  data() {
+    return {
+      link: null
+    }
+  },
+  methods: {
+    async download() {
+      const data = {
+        youtube_url: this.link
+      };
+      try {
+        let response = await fetch(`https://yt-audio-backend-fskt.onrender.com/download_audio`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+        console.log(response);
+
+      } catch (error) {
+        console.log(error);
+
+      }
+    },
+    async startTheApp() {
+      let response = await fetch(`https://yt-audio-backend-fskt.onrender.com/`);
+      console.log(response);
+      console.log('started!');
+
+    }
+  },
+  mounted() {
+    this.startTheApp();
+  }
+}
 </script>
 
 <style scoped></style>
